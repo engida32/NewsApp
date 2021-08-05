3a5a16830421ae93cd22c2d48603b62dcaa7b681
engida32 <engida32lishan@gmail.com>
2021-08-06 00:51:19 +0300

    navBar bloc added

---
+++ lib/bloc/bottom_navbar_bloc.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+enum NavBarItem { HOME, SOURCE, SEARCH }
+
+class NAvBarItemBlock {
+  final StreamController<NavBarItem> navBarController =
+      StreamController<NavBarItem>.broadcast();
+  NavBarItem defaultItem = NavBarItem.HOME;
+  Stream<NavBarItem> get itemStream => navBarController.stream;
+
+  void pickItem(int i) {
+    switch (i) {
+      case 0:
+        navBarController.sink.add(NavBarItem.HOME);
+        break;
+      case 1:
+        navBarController.sink.add(NavBarItem.SOURCE);
+        break;
+      case 2:
+        navBarController.sink.add(NavBarItem.SEARCH);
+
+        break;
+    }
+  }
+}
